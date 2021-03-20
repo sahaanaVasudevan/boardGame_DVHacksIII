10f6369147cb7e08b6fdd7ea8af80e15bfe23aae
Sahaana Vasudevan <sahaana257@gmail.com>
2021-03-19 21:04:05 -0700

    Table view
    Added table view

--- app.py
+++ app.py
@@ -3,3 +3,14 @@ from flask import Flask, render_template,url_for,redirect,request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import random
+
+app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+@app.route('/')
+def home():
+    return render_template('boardGame.html')
+
+if __name__ == "__main__":
+    app.run()
