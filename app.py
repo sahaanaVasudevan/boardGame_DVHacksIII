7a8707b8196db1dd98937e3a211855a7e90b05d6
Sahaana Vasudevan <sahaana257@gmail.com>
2021-03-20 20:29:42 -0700

    Added home and rules page
    Added home and rules page and fixed game board

--- app.py
+++ app.py
@@ -9,8 +9,33 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 @app.route('/')
-def home():
-    return render_template('boardGame.html')
+def game_redirect():
+    return redirect(url_for('index'))
+
+@app.route('/index')
+def index():
+    return render_template("homePage.html")
+
+
+
+@app.route('/funSpree')
+def funSpree():
+    dice_number=random.randint(1,6)
+    dice_number=str(dice_number)
+    url_path='static'+'/'+dice_number+'.jpg'
+    return render_template("boardGame.html",dice_number=dice_number,url_path=url_path)
+
+@app.route('/funSpreeGameRules')
+def funSpreeGameRules():
+    return render_template("funSpreeGameRules.html")
+
+@app.route('/rules')
+def rules_redirect():
+    return redirect(url_for('rules'))
+
+@app.route('/game/rules')
+def rules():
+    return render_template("rules.html")
 
 if __name__ == "__main__":
     app.run()
